f2b5f6d6c479fd47c93d98c5d3205cc331297a69
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2020-03-21 17:56:33 -0300

    finalizado campo minado

--- lib/screens/campo_minado_app.dart
+++ lib/screens/campo_minado_app.dart
@@ -1,49 +1,87 @@
+import 'package:campo_minado/components/tabuleiro_widget.dart';
 import 'package:campo_minado/models/explosao_exception.dart';
+import 'package:campo_minado/models/tabuleiro.dart';
 import 'package:flutter/material.dart';
 import '../components/resultado_widget.dart';
-import '../components/campo_widget.dart';
 import '../models/campo.dart';
 
-class CampoMinadoApp extends StatelessWidget {
-  
+class CampoMinadoApp extends StatefulWidget {
+  @override
+  _CampoMinadoAppState createState() => _CampoMinadoAppState();
+}
+
+class _CampoMinadoAppState extends State<CampoMinadoApp> {
+  bool _venceu;
+  Tabuleiro _tabuleiro;
+
   void _reiniciar() {
-    print('reiniciar');
+    setState(() {
+      _venceu = null;
+      _tabuleiro.reiniciar();
+    });
   }
 
   void _abrir(Campo campo) {
-    print('abrir');
+    if(_venceu != null) {
+      return;    
+    }
+    setState(() {
+      try {
+        campo.abrir();
+        if (_tabuleiro.resolvido) {
+          _venceu = true;
+        }
+      } on ExplosaoException {
+        _venceu = false;
+        _tabuleiro.revelarBombas();
+      }
+    });
   }
 
   void _alterarMarcacao(Campo campo) {
-    print('alterar....');
+    if(_venceu != null) {
+      return;    
+    }
+    setState(() {
+      campo.alternarMarcacao();
+      if (_tabuleiro.resolvido) {
+        _venceu = true;
+      }
+    });
+  }
+
+  Tabuleiro _getTabuleiro(double largura, double altura) {
+    if(_tabuleiro == null) {
+      int qtdeColunas = 15;
+      double tamanhoCampo = largura / qtdeColunas;
+      int qtdeLinhas = (altura / tamanhoCampo).floor();
+
+      _tabuleiro = Tabuleiro(linhas: qtdeLinhas, colunas: qtdeColunas, qtdeBombas: 50);
+    }
+    return _tabuleiro;
   }
 
   @override
   Widget build(BuildContext context) {
-    Campo vizinho1 = Campo(linha: 1, coluna: 0);
-    vizinho1.minar();
-
-    Campo campo = Campo(linha: 0, coluna: 0);
-    //campo.adicionarVizinho(vizinho1);
-    
-    try {
-      campo.alternarMarcacao();
-      //campo.abrir();
-    } on ExplosaoException {
-
-    }
-
     return MaterialApp(
       home: Scaffold(
         appBar: ResultadoWidget(
-          venceu: false,
+          venceu: _venceu,
           onReiniciar: _reiniciar,
         ),
         body: Container(
-          child: CampoWidget(
-            campo: campo,
-            onAbrir: _abrir,
-            onAlterarMarcacao: _alterarMarcacao,
+          color: Colors.grey,
+          child: LayoutBuilder(
+            builder: (ctx, constraints) {
+              return TabuleiroWidget(
+                tabuleiro: _getTabuleiro(
+                  constraints.maxWidth, 
+                  constraints.maxHeight,
+                ),
+                onAbrir: _abrir,
+                onAlterarMarcacao: _alterarMarcacao,
+              );
+            },
           ),
         ),
       ),
